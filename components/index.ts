bc1eedae0b1342f2413d05d71a21e203a5bbb6c7
drekky <130186719+drekky@users.noreply.github.com>
2023-12-20 02:30:32 -0500

    Added fully functional project to repository

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Camp from "./Camp"
+import Features from "./Features"
+import Hero from "./Hero"
+import GetApp from "./GetApp";
+import Guide from "./Guide"
+import Button from "./Button";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+
+export { Camp, Features, Hero, GetApp, Guide, Button, Navbar, Footer }
